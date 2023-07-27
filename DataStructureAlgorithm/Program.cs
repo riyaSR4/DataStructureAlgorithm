08a7af852e90079644fe70c8ccd39aa2b3806a66
Riya Susan Reji <riyasusan.r@tvsnext.com>
2023-07-27 13:05:25 +0530

    [Riya] Add : Replace String

--- DataStructureAlgorithm/Program.cs
+++ DataStructureAlgorithm/Program.cs
@@ -10,7 +10,8 @@ namespace DataStructureAlgorithm
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Enter the option to execute\n 1.Binary Search\n 2.Check Anagram or not\n 3.Exit");
+                Console.WriteLine("Enter the option to execute\n 1.Binary Search\n 2.Check Anagram or not\n " +
+                    "3.Replace String\n 4.Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -23,6 +24,10 @@ namespace DataStructureAlgorithm
                         anagram.CheckAnagram();
                         break;
                     case 3:
+                        ReplaceString replaceString = new ReplaceString();
+                        replaceString.StringReplace();
+                        break;
+                    case 4:
                         flag = false;
                         break;
                 }
---
+++ DataStructureAlgorithm/ReplaceString.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAlgorithm
+{
+    public class ReplaceString
+    {
+        public void StringReplace()
+        {
+            string line = "Hello <<name>>, We have your fullname as << full name >> in our system. your contact number is 91 - xxxxxxxxxx. " +
+                "Please,let us know in case of any clarification Thank you BridgeLabz 01 / 01 / 2016.";
+            Console.WriteLine("Enter name:");
+            string name = Console.ReadLine();
+            line = line.Replace("<<name>>", name);
+            Console.WriteLine("Enter full name:");
+            string fullName = Console.ReadLine();
+            line = line.Replace("<< full name >>", fullName);
+            Console.WriteLine("Enter phone number:");
+            string number = Console.ReadLine();
+            line = line.Replace("xxxxxxxxxx", number);
+            Console.WriteLine("Final Result:" + line);
+        }
+    }
+}
